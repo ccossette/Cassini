f3d36d5ef63d54917d5f7a66fee4df388617f8b6
Carlo Cossette <carlocossette@gmail.com>
2015-06-08 20:51:37 -0700

    Adding the spinning wheel icon and multithreading

--- Cassini/ImageViewController.swift
+++ Cassini/ImageViewController.swift
@@ -22,17 +22,33 @@ class ImageViewController: UIViewController, UIScrollViewDelegate
         }
     }
     
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     private func fetchImage()
     {
         if let url = imageURL
         {
-            let imageData = NSData(contentsOfURL: url)
-            if imageData != nil
+            spinner?.startAnimating()
+            let qos = Int(QOS_CLASS_USER_INITIATED.value)
+            dispatch_async(dispatch_get_global_queue(qos, 0))
             {
-                image = UIImage(data: imageData!)
-            } else
-            {
-                image = nil
+                () -> Void in
+                let imageData = NSData(contentsOfURL: url)
+                dispatch_async(dispatch_get_main_queue())
+                    {
+                    if url == self.imageURL
+                    {
+                        if imageData != nil
+                        {
+                            self.image = UIImage(data: imageData!)
+                        
+                        } else
+                            {
+                                self.image = nil
+                            }
+                    } 
+                }
             }
         }
     }
@@ -63,6 +79,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate
             imageView.image = newValue
             imageView.sizeToFit()
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating()
         }
     }
     
