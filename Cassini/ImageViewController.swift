2388a6ba2d10d13da136fbf1cf619acb83419e20
ccossette <carlocossette@Carlo-Cossettes-MacBook-Pro.local>
2015-06-08 17:25:27 -0700

    Initial commit

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Carlo Cossette on 2015-06-07.
+//  Copyright (c) 2015 C Cossette. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
+    var imageURL: NSURL?
+    {
+        didSet
+        {
+            image = nil
+            if view.window != nil
+            {
+            fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage()
+    {
+        if let url = imageURL
+        {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil
+            {
+                image = UIImage(data: imageData!)
+            } else
+            {
+                image = nil
+            }
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    {
+        didSet
+        {
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+            
+        }
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage?
+    {
+        get { return imageView.image }
+        set
+        {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+            }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil
+        {
+            fetchImage()
+        }
+    }
+    
+}
